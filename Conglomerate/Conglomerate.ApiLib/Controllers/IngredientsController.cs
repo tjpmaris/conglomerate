ea9804face8afb6c6f6d47fe80833f3f66eb867f
tjpmaris <tjpmaris@gmail.com>
2019-12-21 13:39:17 -0600

    Feat: Add barebones api

---
+++ Conglomerate/Conglomerate.ApiLib/Controllers/IngredientsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conglomerate.ApiLib
+{
+    [Route("api/[controller]")]
+    public class IngredientsController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
