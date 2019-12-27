abb388f1f57a03511a2e3ed877b39e26beaa3bff
Tryston Maris <tryston.maris@dialexa.com>
2019-12-27 14:26:38 -0600

    Feat: Test out sending to printer

---
+++ Conglomerate/Conglomerate.Printing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Conglomerate.Printing
+{
+    public class Program
+    {
+        // This is the main entry point for the application.
+        public static void Main(string[] args)
+        {
+            var program = "C:\\Users\\tryst\\AppData\\Local\\PdfToPrinter\\print.exe";
+            var doc = "\"C:\\code\\conglomerate\\Conglomerate\\Conglomerate.Printing\\Documents\\Pdf.pdf\"";
+            var printer = "\"Virtual Printer\"";
+            var command = $"{doc} {printer}";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = program,
+                Arguments = command,
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+
+            var process = Process.Start(startInfo);
+            process.WaitForInputIdle();
+            process.CloseMainWindow();
+        }
+    }
+}
